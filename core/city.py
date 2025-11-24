c20862c5c7b070f285751519a49b3e814a9dc330
Arsheyam Khattak <arsheyam.ktk@gmail.com>
2025-11-24 16:52:08 +0500

    Added city functions

---
+++ core/city.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class City:
+    def __init__():
+        pass
+    
+    def load():
+        pass
+
+    def get_value():
+        pass
+
+    def is_walkable():
+        pass
+
+    def to_matrix():
+        pass
